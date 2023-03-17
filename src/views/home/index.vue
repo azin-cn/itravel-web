248d1b71ecf78c615a3edf2fd3950bc067301e5d
azin-cn <Mr.MF@foxmail.com>
2023-03-17 16:36:55 +0800

    feat: update home page

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -2,181 +2,42 @@
   import { ref, onMounted } from 'vue';
   import * as echarts from 'echarts';
   import { EChartsResizeOption } from 'echarts';
-  import type { FeatureCollection } from '@/types/geo';
   import CN from '@/assets/map/china_without_south_sea.json';
+  import { getSeriesData } from '@/api/map';
+  import type { FeatureCollection } from '@/types/geo';
   import useMap from './use-map';
+  import useECharts from './use-echarts';
+  import useSeries from './use-series';
   import IHeader from './components/IHeader.vue';
+  import IFooter from './components/IFooter.vue';
 
   const { getNewMap } = useMap();
+  const { optionsTooltip, optionsGeo, optionsVisualMap } = useECharts();
+  const { genSeriesAndLegends } = useSeries();
   const headerRef = ref();
   const mapRef = ref();
   const geoJson = ref(CN as FeatureCollection);
 
-  const init = () => {
+  const init = async () => {
     const mapEl = echarts.init(mapRef.value);
-
-    const normalStyle = {
-      borderColor: 'rgba(147, 235, 248, 1)',
-      borderWidth: 1,
-      areaColor: {
-        type: 'radial',
-        x: 0.5,
-        y: 0.5,
-        r: 0.8,
-        colorStops: [
-          {
-            offset: 0,
-            color: 'rgba(147, 235, 248, 0)', // 0% 处的颜色
-          },
-          {
-            offset: 1,
-            color: 'rgba(147, 235, 248, .2)', // 100% 处的颜色
-          },
-        ],
-        globalCoord: false, // 缺省为 false
-      },
-      shadowColor: 'rgba(128, 217, 248, 1)',
-      shadowOffsetX: -2,
-      shadowOffsetY: 2,
-      shadowBlur: 10,
-    };
+    const { series, legends } = await genSeriesAndLegends();
 
     const options = {
       // 背景颜色
       backgroundColor: '#404a59',
+      // backgroundColor: '#4195a4',
       animation: true,
       animationDuration: 1000,
       animationDurationUpdate: 1000,
       // 提示浮窗样式
-      tooltip: {
-        show: true,
-        trigger: 'item',
-        alwaysShowContent: false,
-        backgroundColor: '#0C121C',
-        borderColor: 'rgba(0, 0, 0, 0.16);',
-        hideDelay: 100,
-        triggerOn: 'mousemove',
-        enterable: true,
-        textStyle: {
-          color: '#DADADA',
-          fontSize: 12,
-          width: 20,
-          height: 30,
-          overflow: 'break',
-        },
-        showDelay: 30,
-      },
-      // 地图配置
-      geo: {
-        map: 'china',
-        roam: 'move',
-        left: '13%',
-        top: 'center',
-        // zoom: 1,
-        scaleLimit: {
-          min: 0.5,
-          max: 20,
-        },
-        label: {
-          // 文字通常状态下的样式
-          normal: {
-            show: true,
-            textStyle: {
-              color: '#fff',
-            },
-          },
-          // 文字在鼠标放上去的样式
-          emphasis: {
-            textStyle: {
-              color: '#fff',
-            },
-          },
-        },
-        // 地图区域的样式设置
-        itemStyle: {
-          // 正常状态下的显示
-          normal: normalStyle,
-          // 鼠标放上去高亮的样式
-          emphasis: {
-            areaColor: '#389BB7',
-            borderWidth: 0,
-          },
-        },
-        // 点击选中时的颜色
-        select: {
-          label: {
-            color: '#fff',
-          },
-          itemStyle: normalStyle,
-        },
-
-        // 区域的特殊配置
-        // regions: [
-        //   // 南海诸岛JSON或图片二选一，echarts在options.geo.map为china字符串时自动显示南海诸岛图片
-        //   // {
-        //   //   name: '南海诸岛',
-        //   //   itemStyle: {
-        //   //     // 隐藏地图
-        //   //     normal: {
-        //   //       opacity: 0, // 为 0 时不绘制该图形
-        //   //     },
-        //   //   },
-        //   //   label: {
-        //   //     show: false, // 隐藏文字
-        //   //   },
-        //   // },
-        // ],
-      },
-
-      series: [
-        // {
-        //   type: 'scatter',
-        //   coordinateSystem: 'geo',
-        //   symbol: 'pin',
-        //   legendHoverLink: true,
-        //   symbolSize: [60, 60],
-        //   // 这里渲染标志里的内容以及样式
-        //   label: {
-        //     show: true,
-        //     // formatter(value) {
-        //     //   return value.data.value[2];
-        //     // },
-        //     color: '#fff',
-        //   },
-        //   // 标志的样式
-        //   itemStyle: {
-        //     color: 'rgba(255,0,0,.7)',
-        //     shadowBlur: 2,
-        //     shadowColor: 'D8BC37',
-        //     // normal: {
-        //     // },
-        //   },
-        //   // 数据格式，其中name,value是必要的，value的前两个值是数据点的经纬度，其他的数据格式可以自定义
-        //   // 至于如何展示，完全是靠上面的formatter来自己定义的
-        //   data: [
-        //     { name: '西藏', value: [91.23, 29.5, 2333] },
-        //     { name: '黑龙江', value: [128.03, 47.01, 1007] },
-        //   ],
-        //   showEffectOn: 'render',
-        //   rippleEffect: {
-        //     brushType: 'stroke',
-        //   },
-        //   hoverAnimation: true,
-        //   zlevel: 1,
-        // },
-        {
-          type: 'map',
-          geoIndex: 0,
-          data: [
-            {
-              name: '广东',
-              value: 20,
-            },
-          ],
-        },
-      ],
-
-      // 水印类型，文档：https://echarts.apache.org/zh/option.html#graphic.elements-group
+      tooltip: optionsTooltip,
+      // geo 地图
+      geo: optionsGeo,
+      // 图例
+      legend: legends,
+      // 序列
+      series,
+      // 水印，文档：https://echarts.apache.org/zh/option.html#graphic.elements-group
       graphic: [
         {
           type: 'text', // group text
@@ -198,77 +59,109 @@
           },
         },
       ],
+      // 视觉映射
+      visualMap: optionsVisualMap,
+    } as any; // ECharts的TS类型还有很多问题，大部分定义为object
 
-      visualMap: {
-        show: true, // 是否展示左下角，即是是false，也仅是不显示，不影响数据的映射
-        text: ['高', '低'], // 上下端文字
-        min: 0, // 最小值和最大值，必须指定
-        max: 6000,
-        left: '10%', // 位置
-        bottom: '10%',
-        calculable: true, // 是否展示滑块
-        seriesIndex: [0], // 指定映射的数据，对应的是options.series，这里根据自己的实际需要进行配置
-        inRange: {
-          color: ['#00467F', '#A5CC82'], // 从下到上的颜色
-        },
-        textStyle: {
-          color: '#fff', // 字体颜色
-          map: 'china',
-        },
-      },
-    } as any; // ECharts的TS类型还有很多问题，大部分定义为object，如果遇到了不符合常理的难题，使用any
+    const setSeriesData = (data: any) => {
+      options.series[0].data = data;
+    };
+    const setLegendSelected = (selected: any) => {
+      options.legend.forEach((item: any) => {
+        item.selected = selected;
+      });
+    };
 
-    // 第一个参数必须与 options.geo.map 一致
-    echarts.registerMap('china', CN as object);
-    mapEl.setOption(options);
+    /**
+     *
+     * @param mapName mapName options.geo.map
+     * @param options EChartOption
+     * @param geoJSON FeatureCollection
+     * @param notMerge boolean
+     */
+    const render = (
+      mapName: string,
+      customerOptions: any,
+      geoJSON: FeatureCollection,
+      notMerge = true
+    ) => {
+      // 第一个参数必须与 options.geo.map 一致
+      echarts.registerMap(mapName, geoJSON);
+      mapEl.setOption(customerOptions, notMerge);
+    };
+    render('china', options, CN as FeatureCollection);
 
-    mapEl.off('click'); // 防止graph里频繁添加click事件，在添加click事件之前先全部清空掉
+    /**
+     * function clear
+     * 点击某一个 graphic 节点时，此 graphic 删除后续的节点
+     */
+    const clear = (area: string) => {
+      const index = options.graphic.findIndex(
+        (item: any) => item.style.text === area
+      );
+      options.graphic.splice(index + 1);
+    };
 
-    // 监听所有的echarts.click事件，on('click', () => {})，可单独指定 on('click', 'geo', () => {})
     // mapEl.on('click', console.log);
+    mapEl.off('click'); // 防止graph里频繁添加click事件，在添加click事件之前先全部清空掉
     mapEl.on('click', 'series', async (params: any) => {
       const { name } = params;
       // new map json
       const json = await getNewMap(name, geoJson.value);
+
       if (Object.keys(json).length === 0) {
+        console.log('click series, no json');
         // error message
         return;
       }
 
-      const clear = () => {
-        // 当点击完成后，需要删除后续的节点
-        const index = options.graphic.findIndex(
-          (item: any) => item.style.text === name
-        );
-        options.graphic.splice(index + 1);
-      };
-
-      // exec(); 应放在options.graphic.push后执行，因为graphic需要更新
-      const exec = () => {
-        clear(); // 先清理，后更新
-        echarts.registerMap(name, json);
+      /**
+       * function update 应放在 options 更新后 如 options.graphic.push 后执行，因为 echarts 通过 setOptions 进行更新，需要注意闭包
+       * @param void
+       * @return void
+       */
+      const update = () => {
+        // 先清理，后更新
+        clear(name);
+        // 更新 options.geo
         options.geo.zoom = 0.8;
         options.geo.map = name;
-        mapEl.setOption(options, true); // rerender, it has not animation, see https://github.com/apache/echarts/issues/14069
-        geoJson.value = json; // update
+        // update and remember
+        geoJson.value = json;
+        // rerender, it has not animation, see https://github.com/apache/echarts/issues/14069
+        render(name, options, json, true);
       };
 
       // 将onclick的操作记录到graphic中
       options.graphic.push({
-        type: 'text', // group text
+        type: 'text',
         left: (options.graphic.length + 1) * 60, // 进行偏移，从1开始，所以需要加上1
         top: 60,
-        // 样式设置
         style: {
           text: name, // 文本内容
-          font: 'normal 1rem "Microsoft YaHei", sans-serif', // 字体粗细、大小、字体
-          fill: '#fff', // 字体颜色
+          font: 'normal 1rem "Microsoft YaHei", sans-serif',
+          fill: '#fff',
         },
-        onclick: exec, // graphic具有onclick属性，当点击指定的graphic时，恢复原有的map
+        onclick: update, // 当点击指定的graphic时，恢复原有的map
       });
 
-      exec();
+      update();
     });
+
+    const legendselectchanged = async (params: any) => {
+      const { selected } = params;
+      setLegendSelected(selected);
+
+      // 请求api重新渲染更新数据
+      const { data } = await getSeriesData();
+      setSeriesData(data);
+      mapEl.setOption(options, true);
+    };
+
+    // 监听图列点击，作为筛选条件发送给后台后获取新数据重新渲染
+    mapEl.on('legendselectchanged', legendselectchanged);
+    mapEl.on('legendselectall', legendselectchanged);
+    mapEl.on('legendinverseselect', legendselectchanged);
 
     const autoResize = (): EChartsResizeOption => {
       const { height } = window.getComputedStyle(headerRef.value.$el);
@@ -286,17 +179,210 @@
     });
   };
 
+  const formatChars = (str: string, maxLen = 128) => {
+    if (str.length > maxLen) {
+      return `${str.substring(0, maxLen - 3)}...`;
+    }
+    return str;
+  };
+
   onMounted(async () => {
     init();
   });
 </script>
 
 <template>
-  <!-- 2xl:container mx-auto  -->
-  <div style="width: 100vw; overflow-x: hidden">
+  <!--   -->
+  <div style="width: 100vw">
     <IHeader ref="headerRef" style="background-color: rgb(141 170 185)" />
-    <!-- 99.99 100%会自动溢出，暂时找不到原因 -->
-    <div ref="mapRef" />
+    <div id="echarts" ref="mapRef" style="border-radius: 20%" />
   </div>
-  <!-- <div style="height: 200px"></div> -->
+
+  <div class="2xl:container mx-auto">
+    <section>
+      <h2 class="text-xl m-12"> 热门旅游景点</h2>
+      <!-- 主要景点 -->
+      <div class="hero min-h-0 p-12 pt-6">
+        <div class="hero-content flex-col lg:flex-row-reverse">
+          <img
+            src="../../assets/images/C.jpg"
+            class="max-w-lg rounded-lg shadow-2xl hover-img"
+            width="448"
+            height="290"
+            style="display: block; width: 50%; height: 290px; object-fit: cover"
+          />
+          <div>
+            <h2 class="text-4xl font-bold"> 西藏布达拉宫 </h2>
+            <p class="py-6" style="text-indent: 2em">
+              布达拉宫的主体建筑群坐落在海拔3700多米的玛布日山上，建于公元7世纪，历经唐、宋、元、明、清等多个朝代的修建和扩建，是世界上现存规模最大、最完整的古代宫殿建筑之一，被誉为“世界屋脊”的象征。
+              布达拉宫的主体建筑群坐落在海拔3700多米的玛布日山上，建于公元7世纪，历经唐、宋、元、明、清等多个朝代的修建和扩建，是世界上现存规模最大、最完整的古代宫殿建筑之一，被誉为“世界屋脊”的象征。
+            </p>
+            <button class="btn btn-primary">Get Started</button>
+          </div>
+        </div>
+      </div>
+
+      <!-- 次要的两个景点 -->
+      <div
+        class="flex justify-around p-12 max-w-screen-xl mx-auto"
+        style="height: 340px"
+      >
+        <!-- 一 -->
+        <div class="card md:card-side bg-base-100 p-2" style="width: 54%">
+          <img
+            src="../../assets/images/B.jpg"
+            class="rounded-lg shadow-xl hover-img"
+            alt="Movie"
+            style="width: 52%"
+            loading="lazy"
+          />
+          <div class="card-body">
+            <h2 class="card-title">
+              <div class="truncate"> 珠峰 </div>
+            </h2>
+            <p style="max-height: 110px; text-indent: 2rem">
+              {{
+                formatChars(
+                  '珠穆朗玛峰，高度8844.43米，为海拔世界第一高峰。穆朗玛峰，简称峰，高度8844.43米，为海拔世界第一高峰。'
+                )
+              }}
+            </p>
+            <div class="card-actions justify-end">
+              <button class="btn btn-primary">Watch</button>
+            </div>
+          </div>
+        </div>
+        <!-- 二 -->
+        <div class="card md:card-side bg-base-100 p-2" style="width: 44%">
+          <img
+            src="../../assets/images/A.jpg"
+            class="rounded-lg shadow-xl hover-img"
+            style="width: 48%"
+            alt="Movie"
+          />
+          <div class="card-body">
+            <h2 class="card-title">
+              <div class="truncate"> 俯瞰纳木错 </div>
+            </h2>
+            <p style="max-height: 110px; text-indent: 2rem">
+              “纳木错”为藏语，蒙古语名称为“腾格里海”，都是“天湖”之意。纳木错是西藏的“三大圣湖”之一，也是古象雄雍仲苯教的第一神湖。
+            </p>
+            <div class="card-actions justify-end">
+              <button class="btn btn-primary">Watch</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section>
+      <h2 class="text-xl m-12"> 更多旅游景点 </h2>
+      <div class="flex justify-around">
+        <div
+          class="card w-1/4 bg-base-100 p-4 hover-card"
+          style="height: 380px"
+        >
+          <img
+            src="../../assets/images/A.jpg"
+            class="shadow-xl h-1/2 rounded-lg"
+            style="object-fit: cover"
+            alt="Shoes"
+          />
+          <div class="card-body">
+            <h2 class="card-title">Shoes!</h2>
+            <p>If a dog chews shoes whose shoes does he choose?</p>
+            <div class="card-actions justify-end">
+              <button class="btn btn-primary">Watch</button>
+            </div>
+          </div>
+        </div>
+        <div
+          class="card w-1/4 bg-base-100 p-4 hover-card"
+          style="height: 380px"
+        >
+          <img
+            src="../../assets/images/A.jpg"
+            class="shadow-xl h-1/2 rounded-lg"
+            style="object-fit: cover"
+            alt="Shoes"
+          />
+          <div class="card-body">
+            <h2 class="card-title">Shoes!</h2>
+            <p>If a dog chews shoes whose shoes does he choose?</p>
+            <div class="card-actions justify-end">
+              <button class="btn btn-primary">Watch</button>
+            </div>
+          </div>
+        </div>
+        <div
+          class="card w-1/4 bg-base-100 p-4 hover-card"
+          style="height: 380px"
+        >
+          <img
+            src="../../assets/images/A.jpg"
+            class="shadow-xl h-1/2 rounded-lg"
+            style="object-fit: cover"
+            alt="Shoes"
+          />
+          <div class="card-body">
+            <h2 class="card-title">Shoes!</h2>
+            <p>If a dog chews shoes whose shoes does he choose?</p>
+            <div class="card-actions justify-end">
+              <button class="btn btn-primary">Watch</button>
+            </div>
+          </div>
+        </div>
+        <div
+          class="card w-1/4 bg-base-100 p-4 hover-card"
+          style="height: 380px"
+        >
+          <img
+            src="../../assets/images/A.jpg"
+            class="shadow-xl h-1/2 rounded-lg"
+            style="object-fit: cover"
+            alt="Shoes"
+          />
+          <div class="card-body">
+            <h2 class="card-title">Shoes!</h2>
+            <p>If a dog chews shoes whose shoes does he choose?</p>
+            <div class="card-actions justify-end">
+              <button class="btn btn-primary">Watch</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  </div>
+
+  <IFooter></IFooter>
 </template>
+
+<style scoped>
+  /* less 结合 tailwind 会出现问题 */
+  .card-title {
+    min-width: 0;
+    max-width: 200px;
+  }
+
+  .truncate {
+    overflow: hidden;
+    white-space: nowrap;
+    text-overflow: ellipsis;
+  }
+
+  .hover-card,
+  .hover-img {
+    transition: all 0.2s;
+  }
+
+  .hover-card:hover {
+    @apply w-1/2;
+  }
+
+  .hover-img:hover {
+    transform: scale(1.2);
+  }
+
+  .overflow-ellipsis {
+  }
+</style>
