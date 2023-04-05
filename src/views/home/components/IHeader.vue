6351f27dd7d303d7d59568d29e6425e21e3b9191
azin-cn <Mr.MF@foxmail.com>
2023-04-05 15:17:22 +0800

    feat: add IHeader component

--- src/views/home/components/IHeader.vue
+++ src/views/home/components/IHeader.vue
@@ -1,65 +1,92 @@
 <script lang="ts" setup>
+  import { ref, computed } from 'vue';
+  import { useUserStore } from '@/store';
+  import { isLogin as isLoginFunc } from '@/utils/auth';
   import * as settings from '@/config/settings.json';
+  import avatar from '@/assets/images/avatar.jpg';
+  import avatarNoLogin from '@/assets/images/avatar-no-login.png';
+  import ModalForm from './modal-form.vue';
+
+  const userStore = useUserStore();
+
+  const modalRef = ref<InstanceType<typeof ModalForm>>();
+
+  const isLogin = computed(() => {
+    return isLoginFunc();
+  });
+
+  const onSearch = () => {
+    modalRef.value?.init();
+  };
+
+  const onLogout = async () => {
+    await userStore.logout();
+    window.location.reload();
+  };
 </script>
 
 <template>
-  <div
-    class="navbar bg-opacity-50 pl-36 pr-36 pt-4 pb-4"
-    style="position: sticky; top: 0; z-index: 1"
-  >
-    <div class="flex-1">
-      <!-- Title -->
-      <router-link to="/" class="btn btn-ghost normal-case text-2xl">
-        {{ settings?.title }}
-      </router-link>
-
-      <div class="tabs md:space-x-8 md:ml-24">
-        <router-link to="/home" class="router-link tab-active">
-          Home
+  <div style="position: sticky; top: 0; z-index: 1; max-width: 100vw">
+    <div class="navbar bg-opacity-50 pl-36 pr-36 pt-4 pb-4">
+      <div class="flex-1">
+        <!-- Title -->
+        <router-link to="/" class="btn btn-ghost normal-case text-2xl">
+          {{ settings?.title }}
         </router-link>
-        <router-link to="/about" class="router-link"> About </router-link>
-        <router-link to="/explore" class="router-link"> Explore </router-link>
-        <router-link to="/contact" class="router-link"> Contact </router-link>
+
+        <div class="tabs md:space-x-8 md:ml-24">
+          <router-link to="/home" class="router-link tab-active">
+            主页
+          </router-link>
+          <router-link to="/about" class="router-link"> 关于 </router-link>
+          <router-link to="/explore" class="router-link"> 探索 </router-link>
+          <router-link to="/contact" class="router-link"> 联系 </router-link>
+        </div>
+      </div>
+      <div class="flex-none gap-2">
+        <button class="btn btn-ghost btn-circle" @click.stop="onSearch">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            class="h-5 w-5"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+            />
+          </svg>
+        </button>
+        <div class="dropdown dropdown-end">
+          <label tabindex="0" class="btn btn-ghost btn-circle avatar">
+            <div class="w-10 rounded-full">
+              <img
+                :src="isLogin ? userStore.avatar || avatar : avatarNoLogin"
+              />
+            </div>
+          </label>
+          <ul
+            tabindex="0"
+            class="mt-3 p-2 shadow menu menu-compact dropdown-content bg-base-100 rounded-box w-52"
+          >
+            <li>
+              <a class="justify-between">
+                个人主页
+                <span class="badge">New</span>
+              </a>
+            </li>
+            <li v-if="isLogin" @click.stop="onLogout"><a> 个人登出 </a></li>
+            <li v-else><a> 个人登录 </a></li>
+          </ul>
+        </div>
       </div>
     </div>
-    <div class="flex-none gap-2">
-      <button class="btn btn-ghost btn-circle">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          class="h-5 w-5"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-        >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-          />
-        </svg>
-      </button>
-      <div class="dropdown dropdown-end">
-        <label tabindex="0" class="btn btn-ghost btn-circle avatar">
-          <div class="w-10 rounded-full">
-            <img src="../../../assets/images/avatar.jpg" />
-          </div>
-        </label>
-        <ul
-          tabindex="0"
-          class="mt-3 p-2 shadow menu menu-compact dropdown-content bg-base-100 rounded-box w-52"
-        >
-          <li>
-            <a class="justify-between">
-              Profile
-              <span class="badge">New</span>
-            </a>
-          </li>
-          <li><a>Settings</a></li>
-          <li><a>Logout</a></li>
-        </ul>
-      </div>
-    </div>
+    <!-- <Teleport to="body"> -->
+    <ModalForm ref="modalRef"></ModalForm>
+    <!-- </Teleport> -->
   </div>
 </template>
 
