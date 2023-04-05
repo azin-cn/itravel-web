ec53e6cd60d7d1a6a4da8e0cec759d6f397396af
azin-cn <Mr.MF@foxmail.com>
2023-04-05 15:01:37 +0800

    feat: add modal-form

---
+++ src/layout/components/modal-form.vue
@@ -0,0 +1,52 @@
+<script lang="ts" setup>
+  import { ref } from 'vue';
+  import useVisible from '@/hooks/visible';
+  import useLoading from '@/hooks/loading';
+
+  const { visible, setVisible } = useVisible();
+
+  const form = ref({ search: '' as string });
+
+  const onSearch = () => {
+    const { search } = form.value;
+    if (search.trim()) {
+      console.log(search.trim());
+    }
+  };
+
+  const init = async () => {
+    setVisible(true);
+  };
+
+  defineExpose({ init });
+</script>
+
+<template>
+  <div>
+    <a-modal
+      v-model:visible="visible"
+      :footer="false"
+      :closable="false"
+      :modal-style="{
+        borderRadius: '12px',
+      }"
+      simple
+    >
+      <a-input-search
+        v-model="form.search"
+        placeholder="请输入景点、用户或相关分享"
+        allow-clear
+        style="border-color: rgb(227 227 228 / 100%)"
+        @search="onSearch"
+        @press-enter="onSearch"
+      >
+      </a-input-search>
+    </a-modal>
+  </div>
+</template>
+
+<style lang="less" scoped>
+  :deep(.arco-input-wrapper) {
+    background-color: none;
+  }
+</style>
