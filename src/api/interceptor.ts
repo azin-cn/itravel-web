013f820110995b562450ffe71f90a09cac7401d5
azin-cn <Mr.MF@foxmail.com>
2023-04-08 23:10:30 +0800

    chore: remove some comments

--- src/api/interceptor.ts
+++ src/api/interceptor.ts
@@ -17,10 +17,6 @@ if (import.meta.env.VITE_API_BASE_URL) {
 
 axios.interceptors.request.use(
   (config: AxiosRequestConfig) => {
-    // let each request carry token
-    // this example using the JWT token
-    // Authorization is a custom headers key
-    // please modify it according to the actual situation
     const token = getToken();
     if (token) {
       if (!config.headers) {
