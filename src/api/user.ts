9b9da23c4b92cc86c7bb1358924bf41470155838
azin-cn <Mr.MF@foxmail.com>
2023-04-08 15:16:53 +0800

    feat: rename login type to auth type

--- src/api/user.ts
+++ src/api/user.ts
@@ -4,7 +4,7 @@ import type { RouteRecordNormalized } from 'vue-router';
 import { UserState } from '@/store/modules/user/types';
 import { prefix } from './base';
 
-export const LOGIN_TYPE = {
+export const AUTH_TYPE = {
   ACCOUNT: 1,
   MOBILE: 2,
   THIRD: 3,
@@ -12,6 +12,8 @@ export const LOGIN_TYPE = {
 
 export interface AuthData {
   account: string;
+  username: string;
+  email: string;
   password: string;
   phone: string;
   captcha: string;
@@ -33,13 +35,13 @@ export function login(data: Partial<AuthData>) {
   const { type } = data;
   delete data.type;
 
-  if (type === LOGIN_TYPE.ACCOUNT) {
+  if (type === AUTH_TYPE.ACCOUNT) {
     /**
      * 账户登录
      */
     delete data.phone;
     delete data.captcha;
-  } else if (type === LOGIN_TYPE.MOBILE) {
+  } else if (type === AUTH_TYPE.MOBILE) {
     /**
      * 验证码登录
      */
