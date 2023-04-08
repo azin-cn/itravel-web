a5194416310a7e89fdb05d256d6cbc4e9dc93543
azin-cn <Mr.MF@foxmail.com>
2023-04-08 12:00:47 +0800

    feat: add some router utils

--- src/router/utils.ts
+++ src/router/utils.ts
@@ -1,4 +1,45 @@
+import type { RouteParamsRaw } from 'vue-router';
 import router from './index';
+
+export function getRouteQueries() {
+  const { redirect, ...othersQuery } = router.currentRoute.value.query;
+  return { redirect, othersQuery };
+}
+
+export function redirectHome() {
+  const { othersQuery } = getRouteQueries();
+
+  router.push({
+    name: 'filelist',
+    query: othersQuery,
+  });
+}
+
+export function redirectHomeOrDefault() {
+  const { redirect, othersQuery } = getRouteQueries();
+  router.push({
+    name: (redirect as string) || 'filelist',
+    query: {
+      ...othersQuery,
+    },
+  });
+}
+
+export function redirectLogin() {
+  const { othersQuery } = getRouteQueries();
+  router.push({
+    name: 'login',
+    query: othersQuery,
+  });
+}
+
+export function redirectLoginOrDefault() {
+  const { redirect, othersQuery } = getRouteQueries();
+  router.push({
+    name: (redirect as string) || 'login',
+    query: othersQuery,
+  });
+}
 
 /**
  * 重定向到article
@@ -22,4 +63,11 @@ export const redirectUserInfo = (userId: string) => {
   });
 };
 
-export default { redirectArticle, redirectUserInfo };
+export default {
+  redirectHome,
+  redirectHomeOrDefault,
+  redirectLogin,
+  redirectLoginOrDefault,
+  redirectUserInfo,
+  redirectArticle,
+};
