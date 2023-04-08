ab72205bc85d9bee3e9d63a4a2eabdde2643d1dc
azin-cn <Mr.MF@foxmail.com>
2023-04-08 11:57:55 +0800

    feat: add custom types

--- src/store/modules/user/types.ts
+++ src/store/modules/user/types.ts
@@ -1,20 +1,27 @@
 export type RoleType = '' | '*' | 'admin' | 'user';
 export interface UserState {
-  id?: string;
   name?: string;
-  avatar?: string;
   job?: string;
   organization?: string;
   location?: string;
-  email?: string;
   introduction?: string;
   personalWebsite?: string;
   jobName?: string;
   organizationName?: string;
   locationName?: string;
-  phone?: string;
   registrationDate?: string;
   accountId?: string;
   certification?: number;
   role: RoleType;
+
+  // itravel
+  id?: string;
+  username?: string;
+  phone?: string;
+  email?: string;
+  avatar?: string;
+  description?: string;
+  title?: string;
+  createdTime?: string;
+  updatedTime?: string;
 }
