0f73c2e273104b505a475e015e145e50a004432d
azin-cn <Mr.MF@foxmail.com>
2023-04-08 22:45:32 +0800

    feat: update http response type

--- src/api/interceptor.ts
+++ src/api/interceptor.ts
@@ -6,8 +6,8 @@ import { getToken } from '@/utils/auth';
 
 export interface HttpResponse<T = unknown> {
   status: number;
-  msg: string;
-  code: number;
+  errMsg: string;
+  errCode: number;
   data: T;
 }
 
@@ -40,14 +40,14 @@ axios.interceptors.response.use(
   (response: AxiosResponse<HttpResponse>) => {
     const res = response.data;
     // if the custom code is not 200, it is judged as an error.
-    if (res.code && res.code !== 200) {
+    if (res.errCode && res.errCode !== 200) {
       Message.error({
-        content: res.msg || 'Error',
+        content: res.errMsg || 'Error',
         duration: 5 * 1000,
       });
       // 50008: Illegal token; 50012: Other clients logged in; 50014: Token expired;
       if (
-        [50008, 50012, 50014].includes(res.code) &&
+        [50008, 50012, 50014].includes(res.errCode) &&
         response.config.url !== '/api/user/info'
       ) {
         Modal.error({
@@ -63,13 +63,13 @@ axios.interceptors.response.use(
           },
         });
       }
-      return Promise.reject(new Error(res.msg || 'Error'));
+      return Promise.reject(new Error(res.errMsg || 'Error'));
     }
     return res;
   },
   (error) => {
     Message.error({
-      content: error.msg || 'Request Error',
+      content: error.errMsg || 'Request Error',
       duration: 5 * 1000,
     });
     return Promise.reject(error);
