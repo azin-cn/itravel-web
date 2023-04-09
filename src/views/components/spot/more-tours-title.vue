6465cd88b2140b1f63e2aef871070d20298ec973
azin-cn <Mr.MF@foxmail.com>
2023-04-09 16:06:30 +0800

    feat: add more tours title

---
+++ src/views/components/spot/more-tours-title.vue
@@ -0,0 +1,20 @@
+<script lang="ts" setup>
+  export interface IProps {
+    title?: string;
+    onclick?: () => void;
+  }
+  const props = withDefaults(defineProps<IProps>(), {
+    title: '更多旅游景点',
+    onclick: () => {},
+  });
+</script>
+
+<template>
+  <div class="m-12">
+    <h2 class="text-xl link link-hover inline-block" @click.stop="onclick">
+      <IconFont type="icon-zhilupai"></IconFont>
+      {{ title }}
+      <IconFont type="icon-youjiantou4"></IconFont>
+    </h2>
+  </div>
+</template>
