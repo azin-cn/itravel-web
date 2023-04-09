1e9a88b9d714aaeabd2fe43a0f0a3f097b55a9bc
azin-cn <Mr.MF@foxmail.com>
2023-04-09 12:24:54 +0800

    feat: add authorUrl

--- src/layout/components/IFooter.vue
+++ src/layout/components/IFooter.vue
@@ -1,3 +1,7 @@
+<script lang="ts" setup>
+  import * as settings from '@/config/settings.json';
+</script>
+
 <template>
   <footer class="footer footer-center p-10 text-base-content rounded">
     <div class="grid grid-flow-col gap-4">
@@ -30,8 +34,9 @@
           >
             <path
               d="M19.615 3.184c-3.604-.246-11.631-.245-15.23 0-3.897.266-4.356 2.62-4.385 8.816.029 6.185.484 8.549 4.385 8.816 3.6.245 11.626.246 15.23 0 3.897-.266 4.356-2.62 4.385-8.816-.029-6.185-.484-8.549-4.385-8.816zm-10.615 12.816v-8l8 3.993-8 4.007z"
-            ></path></svg
-        ></a>
+            ></path>
+          </svg>
+        </a>
         <a
           ><svg
             xmlns="http://www.w3.org/2000/svg"
@@ -42,20 +47,18 @@
           >
             <path
               d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z"
-            ></path></svg
-        ></a>
+            ></path>
+          </svg>
+        </a>
       </div>
     </div>
     <div>
       <p
-        >Copyright © 2023 - All right reserved by
-        <a
-          href="https://github.com/azin-cn"
-          class="link link-hover"
-          target="_blank"
-          >Az</a
-        ></p
-      >
+        >Copyright © {{ new Date().getFullYear() }} - All right reserved by
+        <a :href="settings.authorUrl" class="link link-hover" target="_blank">
+          Az
+        </a>
+      </p>
     </div>
   </footer>
 </template>
