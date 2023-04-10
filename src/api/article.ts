172a60d3c4cf1833949b253a62972f3b5b0ef1eb
azin-cn <Mr.MF@foxmail.com>
2023-04-10 12:42:27 +0800

    feat: add  article api

---
+++ src/api/article.ts
@@ -0,0 +1,53 @@
+import axios from 'axios';
+import { ListResult } from '@/types/global';
+import { prefix } from './base';
+
+interface Spot {
+  id: string;
+  name: string;
+  description: string;
+}
+
+interface Author {
+  id: string;
+  username: string;
+  avatar: string;
+  description: string | null;
+}
+
+export interface ArticleBriefInfo {
+  id: string;
+  title: string;
+  summary: string;
+  content: string;
+  viewCount: number;
+  likeCount: number;
+  favCount: number;
+  status: number;
+  publishTime: string;
+  createdTime: string;
+  updatedTime: string;
+  spot: Spot;
+  author: Author;
+  commentCount: number;
+}
+
+export interface IPaginationOpton {
+  limit: number;
+  page: number;
+}
+
+/**
+ * 获取文章的简略信息
+ * @param id
+ * @returns
+ */
+export function getSpotBriefInfoOfArticles(
+  id: string,
+  params: Partial<IPaginationOptons> = {}
+) {
+  return axios.get<ListResult<ArticleBriefInfo>>(
+    `${prefix}/article/spot/${id}`,
+    { params }
+  );
+}
