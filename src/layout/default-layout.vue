9028e5bcc1832607f3dd7e413829a8b988a2f691
azin-cn <Mr.MF@foxmail.com>
2023-04-10 18:22:25 +0800

    perf: perfect tag level

--- src/layout/default-layout.vue
+++ src/layout/default-layout.vue
@@ -1,6 +1,4 @@
 <script lang="ts" setup>
-  import { ref, computed, watch, provide, onMounted } from 'vue';
-  import { useRouter, useRoute } from 'vue-router';
   import PageLayout from './page-layout.vue';
   import IHeader from './components/IHeader.vue';
   import IFooter from './components/IFooter.vue';
@@ -12,11 +10,9 @@
       <a-layout-header class="sticky top-0 w-full z-10">
         <IHeader />
       </a-layout-header>
-      <a-layout>
-        <a-layout-content>
-          <PageLayout />
-        </a-layout-content>
-      </a-layout>
+      <a-layout-content>
+        <PageLayout />
+      </a-layout-content>
       <a-layout-footer>
         <IFooter></IFooter>
       </a-layout-footer>
@@ -32,14 +28,14 @@
     display: flex;
     flex-direction: column;
     justify-content: center;
-    color: var(--color-white);
+
+    /* color: var(--color-white); */
     font-size: 16px;
     font-stretch: condensed;
     text-align: center;
   }
 
   .layout :deep(.arco-layout-sider) {
-    width: 206px;
-    background-color: var(--color-primary-light-3);
+    background-color: inherit;
   }
 </style>
