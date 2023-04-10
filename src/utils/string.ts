a26e9c62550b556add721699ea2d6e448dee17b0
azin-cn <Mr.MF@foxmail.com>
2023-04-10 12:47:12 +0800

    chore: rename string util to format util

--- src/utils/string.ts
+++
@@ -1,8 +0,0 @@
-export function limitLength(min: number, max: number) {}
-
-export function limitMaxLength(str: string, max = 24): string {
-  if (str.length > max) {
-    return `${str.substring(0, max)}...`;
-  }
-  return str;
-}
