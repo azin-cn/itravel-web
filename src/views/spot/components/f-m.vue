77042ca85fa5f50259310568fd201549aef914cf
azin-cn <Mr.MF@foxmail.com>
2023-04-10 12:46:09 +0800

    feat: add fm component

---
+++ src/views/spot/components/f-m.vue
@@ -0,0 +1,69 @@
+<script lang="ts" setup>
+  import useFM from '../use-fm';
+
+  export interface IMonthOrFeature {
+    id: string;
+    name: string;
+  }
+  export interface IProps {
+    features: IMonthOrFeature[];
+    months: IMonthOrFeature[];
+    province?: string;
+    city?: string;
+  }
+  const props = defineProps<IProps>();
+
+  const { months: allMonths, features: allFeatures } = useFM();
+
+  const hasMonth = (name: string) => {
+    return props.months.find((item) => item.name === name);
+  };
+  const hasFeature = (name: string) => {
+    return props.features.find((item) => item.name === name);
+  };
+</script>
+
+<template>
+  <section class="text-right">
+    <div v-if="features?.length">
+      <kbd
+        v-for="item in allFeatures"
+        :key="item.name"
+        class="kbd kbd-sm m-1 p-1"
+      >
+        <div class="text-center align-middle">
+          <IconFont v-if="hasFeature(item.name)" type="icon--"></IconFont>
+          <span>{{ item.name }}</span>
+        </div>
+      </kbd>
+    </div>
+
+    <div v-if="months?.length">
+      <kbd
+        v-for="item in allMonths"
+        :key="item.name"
+        class="kbd kbd-sm m-1 p-1"
+      >
+        <div class="text-center align-middle">
+          <IconFont v-if="hasMonth(item.name)" type="icon-zhishipai"></IconFont>
+          <span>{{ item.name }}</span>
+        </div>
+      </kbd>
+    </div>
+
+    <div class="text-right">
+      <kbd v-if="province" class="kbd kbd-sm m-1 p-1">
+        <div class="text-center align-middle">
+          <IconFont type="icon-mianxinglupaizhishipai"></IconFont>
+          <span>{{ province }}</span>
+        </div>
+      </kbd>
+      <kbd v-if="city" class="kbd kbd-sm m-1 p-1">
+        <div class="text-center align-middle">
+          <IconFont type="icon-mianxinglupaizhishipai"></IconFont>
+          <span>{{ city }}</span>
+        </div>
+      </kbd>
+    </div>
+  </section>
+</template>
