70ffd98e7f9df0dc9e6f44402660b1356a3e5315
azin-cn <Mr.MF@foxmail.com>
2023-04-10 13:01:12 +0800

    feat: add article brief component

--- src/views/components/article/article-brief.vue
+++ src/views/components/article/article-brief.vue
@@ -1,5 +1,91 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+  import { limitMaxLength, formatNumber } from '@/utils/format';
+  import { redirectArticle } from '@/router/utils';
+  import { ArticleBriefInfo } from '@/api/article';
+  import useArticle from '@/views/spot/use-article';
+
+  export interface IProps {
+    list: ArticleBriefInfo[];
+    imgUrl?: string;
+  }
+
+  const props = defineProps<IProps>();
+
+  const { onArticleShare } = useArticle();
+</script>
 
 <template>
-  <div> ArticleBreif 点赞、收藏、评论 </div>
+  <div v-if="list?.length">
+    <template v-for="item in list" :key="item.id">
+      <div
+        class="card md:card-side bg-base-100 m-1 p-2 mt-2 mb-2 text-gray-700"
+        style="border: 1px solid #fff; box-shadow: 0 0 2px 0 #ccc"
+      >
+        <img
+          :src="item.thumbUrl || imgUrl"
+          class="rounded-lg shadow-xl hover-img cursor-pointer"
+          alt="Movie"
+          :style="{ width: '24%' }"
+          loading="lazy"
+          @click.stop="redirectArticle(item.id)"
+        />
+        <div class="card-body">
+          <h2
+            class="card-title cursor-pointer link link-hover"
+            @click.stop="redirectArticle(item.id)"
+          >
+            {{ item.title }}
+          </h2>
+
+          <div class="text-left truncate">
+            <p>{{ limitMaxLength(item.summary || item.content) }}</p>
+          </div>
+
+          <div class="text-left link-primary text-sm">
+            <p>
+              <IconFont type="icon-shijian" />
+              {{ new Date(item.publishTime).toLocaleString() }}
+            </p>
+          </div>
+
+          <div class="flex items-center justify-between">
+            <div class="flex items-center">
+              <div class="mr-2">
+                <IconFont
+                  type="icon-pinglun3"
+                  class="cursor-pointer icon-click"
+                  @click.stop="redirectArticle(item.id)"
+                />
+                <span class="text-xs link-neutral">
+                  {{ item.commentCount || formatNumber(12012012) }}
+                </span>
+              </div>
+              <div class="mr-2">
+                <IconFont
+                  type="icon-dianzan3"
+                  class="cursor-pointer icon-click"
+                  @click.stop="() => {}"
+                />
+                <span class="text-xs link-neutral">
+                  {{ item.likeCount || formatNumber(12012012) }}
+                </span>
+              </div>
+              <div class="mr-2" @click="onArticleShare(item.id)">
+                <IconFont
+                  type="icon-zhuanfa2"
+                  size="13"
+                  class="cursor-pointer icon-click"
+                />
+              </div>
+            </div>
+
+            <a-tooltip :content="item.author.username" class="text-right">
+              <a-avatar :size="28" :image-url="item.author.avatar" />
+            </a-tooltip>
+          </div>
+        </div>
+      </div>
+    </template>
+  </div>
+  <div v-else><a-empty /></div>
 </template>
