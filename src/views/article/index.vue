c08670d987aaa6d6a09b8594e7e9dab92d6f4e2a
azin-cn <Mr.MF@foxmail.com>
2023-04-10 22:07:35 +0800

    feat: import sider layout component

--- src/views/article/index.vue
+++ src/views/article/index.vue
@@ -1,9 +1,14 @@
 <script lang="ts" setup>
   import RandRecomSpot from './components/rand-recom-spot.vue';
+  import SiderLayout from '../components/layout/sider-layout.vue';
 </script>
 
 <template>
   <div class="2xl:container mx-auto text-gray-700 text-left">
+    <SiderLayout>
+      <!-- <template #content>这是内容</template> -->
+    </SiderLayout>
+
     <RandRecomSpot></RandRecomSpot>
   </div>
 </template>
