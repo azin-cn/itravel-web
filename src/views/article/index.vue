c23315e3b834d22808ee7a0ecd5880a6527caf50
azin-cn <Mr.MF@foxmail.com>
2023-04-10 21:45:56 +0800

    feat: add rand recom spot component

--- src/views/article/index.vue
+++ src/views/article/index.vue
@@ -1,5 +1,9 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+  import RandRecomSpot from './components/rand-recom-spot.vue';
+</script>
 
 <template>
-  <div>Article</div>
+  <div class="2xl:container mx-auto text-gray-700 text-left">
+    <RandRecomSpot></RandRecomSpot>
+  </div>
 </template>
