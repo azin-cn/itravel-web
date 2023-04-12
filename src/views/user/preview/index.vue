0e1752760bff440b263e75c5f6119cecc3e0f46e
azin-cn <Mr.MF@foxmail.com>
2023-04-13 00:22:03 +0800

    feat: add user component

---
+++ src/views/user/preview/index.vue
@@ -0,0 +1,20 @@
+<script lang="ts" setup>
+  import { useRoute } from 'vue-router';
+  import * as settings from '@/config/settings.json';
+  import SiderLayout from '@/views/components/layout/sider-layout.vue';
+
+  const route = useRoute();
+
+  const init = async () => {
+    const { params } = route;
+    const { userId } = params;
+  };
+
+  document.title = `${settings.title} - `;
+</script>
+
+<template>
+  <div>
+    <SiderLayout></SiderLayout>
+  </div>
+</template>
