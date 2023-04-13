fba251280d3f49a3a1b6cc6a0c1a489b1d381748
azin-cn <Mr.MF@foxmail.com>
2023-04-13 12:28:21 +0800

    feat: update user preview page

--- src/views/user/preview/index.vue
+++ src/views/user/preview/index.vue
@@ -57,113 +57,68 @@
       <template #content>
         <div class="m-2 flex-1">
           <!-- 作者介绍和缩略图 -->
-          <div class="flex items-stretch">
+          <div class="items-stretch">
             <div
-              class="flex flex-col justify-center bg-base-100 p-6 mt-2 mb-2 text-gray-700"
+              class="flex flex-1 justify-center bg-base-100 p-6 mt-2 mb-2 text-gray-700"
               :style="{
                 'border': '1px solid #fff',
                 'box-shadow': '0 0 2px 0 #ccc',
                 'border-radius': '8px',
               }"
             >
-              <div class="flex items-center text-left">
-                <a-avatar
-                  :size="44"
-                  :image-url="userInfo?.avatar"
-                  class="mr-2 cursor-pointer"
-                  @click.stop="onRedirectUserPreview(userInfo?.id as string)"
-                />
-                <div class="flex-1">
-                  <div class="flex items-center justify-between">
-                    <h4 class="text-base">
-                      {{ userInfo?.username }}
-                    </h4>
-                    <span>
-                      <IconFont type="icon-tuijian2" />
-                      <span class="text-xs link-neutral">
-                        {{ userInfo?.visitors || formatNumber(12012) }}
+              <div class="w-1/2 flex items-center self-center">
+                <div class="flex items-center text-left">
+                  <a-avatar
+                    :size="64"
+                    :image-url="userInfo?.avatar"
+                    class="mr-12 cursor-pointer"
+                    @click.stop="onRedirectUserPreview(userInfo?.id as string)"
+                  />
+
+                  <div class="flex-1">
+                    <div class="flex items-center justify-between">
+                      <h4 class="text-2xl truncate" style="max-width: 60%">
+                        {{ userInfo?.username }}
+                      </h4>
+                      <span>
+                        <IconFont type="icon-tuijian2" />
+                        <span class="text-xs link-neutral">
+                          {{
+                            userInfo?.visitors ||
+                            formatNumber(Math.random() * 10000)
+                          }}
+                        </span>
                       </span>
-                    </span>
-                  </div>
-                  <p class="text-xs w-48 truncate">
-                    {{
-                      userInfo?.description || '人之初，性本善，性相近，习相远'
-                    }}
-                  </p>
-                </div>
-              </div>
-
-              <!-- 第一篇文章的点赞 -->
-              <div class="flex items-center mt-10">
-                <div class="mr-2">
-                  <IconFont type="icon-pinglun3" />
-                  <span class="text-xs link-neutral">
-                    {{
-                      userArticles?.list[0].commentCount || formatNumber(120012)
-                    }}
-                  </span>
-                </div>
-                <div class="mr-2">
-                  <IconFont
-                    type="icon-dianzan3"
-                    class="cursor-pointer icon-click"
-                    @click.stop="onThumbsUp(userArticles?.list[0].id as string)"
-                  />
-                  <span class="text-xs link-neutral">
-                    {{
-                      userArticles?.list[0].likeCount || formatNumber(120112)
-                    }}
-                  </span>
-                </div>
-                <div class="mr-2">
-                  <IconFont
-                    type="icon-zhuanfa2"
-                    size="13"
-                    class="cursor-pointer icon-click"
-                    @click.stop="
-                      onArticleShare(userArticles?.list[0].id as string)
-                    "
-                  />
-                </div>
-              </div>
-
-              <!-- 位置 -->
-              <div class="mt-6 text-left">
-                <div class="mr-2">
-                  <IconFont type="icon-nationaarea" class="mr-1" />
-                  <span class="text-sm">
-                    {{
-                      `${spotInfo?.province.name} - ${spotInfo?.city.name}` ||
-                      '暂无数据'
-                    }}
-                  </span>
-                  <div>
-                    <IconFont type="icon-zhishipai" class="mr-1" />
-                    <span class="text-sm">
-                      {{ spotInfo?.name }}
-                    </span>
+                    </div>
+                    <p class="text-xs w-full truncate">
+                      {{
+                        userInfo?.description ||
+                        '人之初，性本善，性相近，习相远'
+                      }}
+                    </p>
+                    <!-- 位置 -->
+                    <div class="mt-3 text-left">
+                      <div class="mr-2">
+                        <IconFont type="icon-nationaarea" class="mr-1" />
+                        <span class="text-sm">
+                          最近去过：{{
+                            `${userArticles?.list[0]?.spot.name || '暂无数据'}`
+                          }}
+                        </span>
+                        <div>
+                          <IconFont type="icon-zhishipai" class="mr-1" />
+                          <span class="text-sm">
+                            最新分享：{{
+                              `${userArticles?.list[0]?.title || '暂无数据'}`
+                            }}
+                          </span>
+                        </div>
+                      </div>
+                    </div>
                   </div>
                 </div>
               </div>
             </div>
-
-            <a-image
-              show-loader
-              fit="cover"
-              class="flex-1 m-2 overflow-hidden h-60 lg:h-80 cursor-pointer"
-              style="border-radius: 8px"
-              :src="
-                userInfo?.thumbUrl ||
-                userArticles?.list[0].thumbUrl ||
-                userArticles?.list[0].spot.thumbUrl
-              "
-              :alt="userArticles?.list[0].title"
-            >
-              <!-- :description="articleInfo?.spot.name" -->
-              <template #extra>
-                <span class="action" @click="() => {}"><icon-download /></span>
-              </template>
-            </a-image>
           </div>
 
           <div class="text-left mt-6 mb-6">
@@ -178,7 +133,7 @@
           </div>
 
           <ArticleBrief
-            :img-url="userArticles?.list[0].thumbUrl"
+            :img-url="userArticles?.list[0]?.spot.thumbUrl"
             :list="userArticles?.list as ArticleBriefInfo[]"
           />
 
