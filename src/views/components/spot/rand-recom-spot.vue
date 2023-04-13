6df9e9b7dab16b40256fc65f48aa28cb484b5779
azin-cn <Mr.MF@foxmail.com>
2023-04-13 10:03:17 +0800

    feat: add rand recom spot component

---
+++ src/views/components/spot/rand-recom-spot.vue
@@ -0,0 +1,16 @@
+<script lang="ts" setup>
+  import MoreToursTitle from './more-tours-title.vue';
+  import MoreTours from './more-tours.vue';
+
+  export interface IProps {
+    line?: 1 | 2;
+  }
+  const prpos = withDefaults(defineProps<IProps>(), { line: 1 });
+</script>
+
+<template>
+  <section>
+    <MoreToursTitle></MoreToursTitle>
+    <MoreTours :line="line"></MoreTours>
+  </section>
+</template>
