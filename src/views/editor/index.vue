e4ad0275249897b27df55761a8549381bec77cd2
azin-cn <Mr.MF@foxmail.com>
2023-04-13 15:07:26 +0800

    feat: add editor page

---
+++ src/views/editor/index.vue
@@ -0,0 +1,9 @@
+<script lang="ts" setup>
+  import SiderLayout from '../components/layout/sider-layout.vue';
+</script>
+
+<template>
+  <div>
+    <SiderLayout></SiderLayout>
+  </div>
+</template>
