5dd647953c4ccb911ddcad6333b38f175115687d
azin-cn <Mr.MF@foxmail.com>
2023-04-13 15:05:17 +0800

    feat: update editor route

--- src/router/routes/modules/editor.ts
+++ src/router/routes/modules/editor.ts
@@ -3,14 +3,18 @@ import { DEFAULT_LAYOUT } from '../base';
 
 export const EDITOR_ROUTE: RouteRecordRaw = {
   path: '/editor',
-  name: '',
   component: DEFAULT_LAYOUT,
   meta: {
     requiresAuth: true,
   },
   children: [
     {
+      path: '',
+      redirect: '/',
+    },
+    {
       path: 'draft/:articleId?',
+      name: 'editor',
       component: () => import('@/views/editor/index.vue'),
       meta: {
         requiresAuth: true,
