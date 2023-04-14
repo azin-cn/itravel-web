9f0392af0341cfd011e5558a611b00fd4017fa5b
azin-cn <Mr.MF@foxmail.com>
2023-04-14 22:45:26 +0800

    feat: add reply mini component

---
+++ src/components/reply-mini/index.vue
@@ -0,0 +1,49 @@
+<script lang="ts" setup>
+  import { ref } from 'vue';
+
+  export interface IProps {
+    minRows: number;
+    maxRows: number;
+    maxLength: number;
+    btnText: string;
+    placeholder: string;
+  }
+
+  const props = withDefaults(defineProps<IProps>(), {
+    minRows: 2,
+    maxRows: 10,
+    maxLength: 100,
+    btnText: '发送',
+    placeholder: '在这里输入你的观点吧！',
+  });
+  const emits = defineEmits(['reply']);
+  const comment = ref('');
+
+  const onReply = () => {
+    const { value } = comment;
+    emits('reply', value);
+  };
+</script>
+
+<template>
+  <div class="mb-6 flex items-center">
+    <a-textarea
+      v-model="comment"
+      :auto-size="{
+        minRows,
+        maxRows,
+      }"
+      :max-length="maxLength"
+      allow-clear
+      show-word-limit
+      :placeholder="placeholder"
+      :style="{
+        'border-color': 'rgb(227 227 228 / 100%)',
+        'border-radius': '6px',
+      }"
+      class="mr-3"
+    >
+    </a-textarea>
+    <a-button @click.stop="onReply">{{ btnText }}</a-button>
+  </div>
+</template>
