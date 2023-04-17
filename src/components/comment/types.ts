8a772993ee34600ee6dc48a2bdface5d6af14b4c
azin-cn <Mr.MF@foxmail.com>
2023-04-17 18:44:24 +0800

    feat: update emit action record

--- src/components/comment/types.ts
+++ src/components/comment/types.ts
@@ -1,8 +1,12 @@
+export interface IRecord {
+  user: string;
+  toUser: string;
+  parent: string;
+  content: string;
+  comment: string;
+}
+
 export interface IAction {
   key: 'reply' | 'thumb-up' | 'delete';
-  record: {
-    userId: string;
-    toUserId: string;
-    content?: string;
-  };
+  record: Partial<IRecord>;
 }
