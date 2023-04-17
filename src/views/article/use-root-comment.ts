bff4b51f0a3c2e270b6a2430c4e00fb2f4cc1dda
azin-cn <Mr.MF@foxmail.com>
2023-04-18 07:49:28 +0800

    feat: add use root comment

---
+++ src/views/article/use-root-comment.ts
@@ -0,0 +1,37 @@
+import { ref, Ref } from 'vue';
+import useLoading from '@/hooks/loading';
+import { ArticleBriefInfo, postComment } from '@/api/article';
+import { Message } from '@arco-design/web-vue';
+import { useUserStore } from '@/store';
+
+export default function useRootComment(
+  article: Ref<ArticleBriefInfo | undefined>
+) {
+  const { loading: rootLoading, setLoading: setRootLoading } = useLoading();
+  const userStore = useUserStore();
+  const rootComment = ref('');
+
+  const resetComment = () => {
+    rootComment.value = '';
+  };
+
+  const onSubmitRootComment = async () => {
+    const { value } = rootComment;
+    setRootLoading(true);
+    try {
+      await postComment({
+        user: userStore.id as string,
+        toUser: article.value?.author.id as string,
+        article: article.value?.id as string,
+        parent: null,
+        content: value,
+      });
+      Message.success('提交成功');
+      resetComment();
+    } finally {
+      setRootLoading(false);
+    }
+  };
+
+  return { rootComment, rootLoading, setRootLoading, onSubmitRootComment };
+}
