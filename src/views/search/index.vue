ee86ad95fe7bd7e4c192179b7cb0072c8b36f314
azin-cn <Mr.MF@foxmail.com>
2023-04-18 23:33:42 +0800

    feat: add sider layout and rand recom spot component

--- src/views/search/index.vue
+++ src/views/search/index.vue
@@ -1,3 +1,12 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+  import { ref } from 'vue';
+  import SiderLayout from '../components/layout/sider-layout.vue';
+  import RandRecomSpot from '../article/components/rand-recom-spot.vue';
+</script>
 
-<template>Search</template>
+<template>
+  <div>
+    <SiderLayout></SiderLayout>
+    <RandRecomSpot></RandRecomSpot>
+  </div>
+</template>
