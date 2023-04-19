745cfd66cce0064306d99ee4fc19de362e7ed9ff
azin-cn <Mr.MF@foxmail.com>
2023-04-19 09:02:11 +0800

    feat: add title tag

--- src/views/search/index.vue
+++ src/views/search/index.vue
@@ -23,7 +23,16 @@
 <template>
   <div class="2xl:container mx-auto text-gray-700 text-left">
     <SiderLayout>
-      <template #content> </template>
+      <template #content>
+        <div class="m-2 flex-1 text-left">
+          <div class="m-6 ml-0">
+            <h2 class="text-lg link link-hover inline-block">
+              <IconFont type="icon-zhilupai"></IconFont>
+              更多
+            </h2>
+          </div>
+        </div>
+      </template>
     </SiderLayout>
     <RandRecomSpot></RandRecomSpot>
   </div>
