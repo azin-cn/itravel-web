f39fe26da5c69be2c62819913cc647fa37361102
azin-cn <Mr.MF@foxmail.com>
2023-04-19 09:20:06 +0800

    feat: update search method

--- src/api/search.ts
+++ src/api/search.ts
@@ -1,5 +1,9 @@
 import axios from 'axios';
+import { ListResult } from '@/types/global';
+import { UserState } from '@/store/modules/user/types';
 import { IPaginationOpton, prefix } from './base';
+import { ArticleBriefInfo } from './article';
+import { ITour, SpotBreifInfoModel } from './spot';
 
 /**
  * 根据关键词搜索用户
@@ -8,9 +12,31 @@ import { IPaginationOpton, prefix } from './base';
  * @returns
  */
 export function getUsersByKeywords(s: string, options: IPaginationOpton) {
-  return axios.get(`${prefix}/search/user`, { params: { s, ...options } });
+  return axios.get<ListResult<UserState>>(`${prefix}/search/user`, {
+    params: { s, ...options },
+  });
 }
 
+/**
+ * 根据关键词搜索景点
+ * @param s
+ * @param options
+ * @returns
+ */
 export function getSpotsByKeywords(s: string, options: IPaginationOpton) {
-  return axios.get(`${prefix}/search/spot`, { params: { s, ...options } });
+  return axios.get<ListResult<ITour>>(`${prefix}/search/spot`, {
+    params: { s, ...options },
+  });
+}
+
+/**
+ * 根据关键词搜索文章
+ * @param s
+ * @param options
+ * @returns
+ */
+export function getArticlesByKeywords(s: string, options: IPaginationOpton) {
+  return axios.get<ListResult<ArticleBriefInfo>>(`${prefix}/search/article`, {
+    params: { s, ...options },
+  });
 }
