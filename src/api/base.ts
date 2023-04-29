f327ada7449e57a870499596b3c6bb99ed6acd77
azin-cn <Mr.MF@foxmail.com>
2023-04-29 16:19:32 +0800

    feat: update url prefix

--- src/api/base.ts
+++ src/api/base.ts
@@ -10,7 +10,7 @@
  */
 export const isProd = process.env.NODE_ENV === 'production';
 export const prefix = isProd
-  ? 'http://localhost:7000'
+  ? 'https://itravel.todayto.com'
   : 'http://localhost:7000';
 
 /**
