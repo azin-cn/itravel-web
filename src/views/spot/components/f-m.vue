ca1ec76c35319eedffacf9d0ba81a2147138739e
azin-cn <Mr.MF@foxmail.com>
2023-04-29 12:47:09 +0800

    feat: use rotate box component

--- src/views/spot/components/f-m.vue
+++ src/views/spot/components/f-m.vue
@@ -1,4 +1,5 @@
 <script lang="ts" setup>
+  import RotateBox from '@/views/components/rotate-box/index.vue';
   import useFM from '../use-fm';
 
   export interface IMonthOrFeature {
@@ -24,46 +25,56 @@
 </script>
 
 <template>
-  <section class="text-right">
-    <div v-if="features?.length">
-      <kbd
-        v-for="item in allFeatures"
-        :key="item.name"
-        class="kbd kbd-sm m-1 p-1"
-      >
-        <div class="text-center align-middle">
-          <IconFont v-if="hasFeature(item.name)" type="icon--"></IconFont>
-          <span>{{ item.name }}</span>
-        </div>
-      </kbd>
-    </div>
+  <div class="flex items-center justify-between">
+    <section class="pl-28">
+      <a-tooltip content="点击进入VR空间">
+        <RotateBox></RotateBox>
+      </a-tooltip>
+    </section>
+    <section class="text-right">
+      <div v-if="features?.length">
+        <kbd
+          v-for="item in allFeatures"
+          :key="item.name"
+          class="kbd kbd-sm m-1 p-1"
+        >
+          <div class="text-center align-middle">
+            <IconFont v-if="hasFeature(item.name)" type="icon--"></IconFont>
+            <span>{{ item.name }}</span>
+          </div>
+        </kbd>
+      </div>
 
-    <div v-if="months?.length">
-      <kbd
-        v-for="item in allMonths"
-        :key="item.name"
-        class="kbd kbd-sm m-1 p-1"
-      >
-        <div class="text-center align-middle">
-          <IconFont v-if="hasMonth(item.name)" type="icon-zhishipai"></IconFont>
-          <span>{{ item.name }}</span>
-        </div>
-      </kbd>
-    </div>
+      <div v-if="months?.length">
+        <kbd
+          v-for="item in allMonths"
+          :key="item.name"
+          class="kbd kbd-sm m-1 p-1"
+        >
+          <div class="text-center align-middle">
+            <IconFont
+              v-if="hasMonth(item.name)"
+              type="icon-zhishipai"
+            ></IconFont>
+            <span>{{ item.name }}</span>
+          </div>
+        </kbd>
+      </div>
 
-    <div class="text-right">
-      <kbd v-if="province" class="kbd kbd-sm m-1 p-1">
-        <div class="text-center align-middle">
-          <IconFont type="icon-mianxinglupaizhishipai"></IconFont>
-          <span>{{ province }}</span>
-        </div>
-      </kbd>
-      <kbd v-if="city" class="kbd kbd-sm m-1 p-1">
-        <div class="text-center align-middle">
-          <IconFont type="icon-mianxinglupaizhishipai"></IconFont>
-          <span>{{ city }}</span>
-        </div>
-      </kbd>
-    </div>
-  </section>
+      <div class="text-right">
+        <kbd v-if="province" class="kbd kbd-sm m-1 p-1">
+          <div class="text-center align-middle">
+            <IconFont type="icon-mianxinglupaizhishipai"></IconFont>
+            <span>{{ province }}</span>
+          </div>
+        </kbd>
+        <kbd v-if="city" class="kbd kbd-sm m-1 p-1">
+          <div class="text-center align-middle">
+            <IconFont type="icon-mianxinglupaizhishipai"></IconFont>
+            <span>{{ city }}</span>
+          </div>
+        </kbd>
+      </div>
+    </section>
+  </div>
 </template>
