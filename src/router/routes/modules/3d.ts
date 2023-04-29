21c6a9bdbc9392dabb916dc4e3c4780c9d5fe8f3
azin-cn <Mr.MF@foxmail.com>
2023-04-29 14:10:56 +0800

    feat: update 3d route

--- src/router/routes/modules/3d.ts
+++ src/router/routes/modules/3d.ts
@@ -1,26 +1,12 @@
 import { RouteRecordRaw } from 'vue-router';
-import { DEFAULT_LAYOUT } from '../base';
 
 export const THREE_ROUTE: RouteRecordRaw = {
-  path: '/3d',
-  component: DEFAULT_LAYOUT,
+  name: '3d',
+  path: '/3d/:spotId',
+  component: () => import('@/views/3d/index.vue'),
   meta: {
     requiresAuth: false,
   },
-  children: [
-    {
-      path: '',
-      redirect: '/home',
-    },
-    {
-      name: '3d',
-      path: ':spotId',
-      component: () => import('@/views/3d/index.vue'),
-      meta: {
-        requiresAuth: false,
-      },
-    },
-  ],
 };
 
 export default THREE_ROUTE;
