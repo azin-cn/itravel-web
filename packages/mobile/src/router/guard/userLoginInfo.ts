48f8f3b4a076891d7d694d4790d9cb0c68877f89
azin-cn <Mr.MF@foxmail.com>
2023-05-03 12:52:47 +0800

    feat: process not login logic

--- packages/mobile/src/router/guard/userLoginInfo.ts
+++ packages/mobile/src/router/guard/userLoginInfo.ts
@@ -3,11 +3,25 @@ import NProgress from 'nprogress'; // progress bar
 
 import { useUserStore } from '@/store';
 import { isLogin } from '@/utils/auth';
+import { LocationQuery } from 'vue-router';
 
 export default function setupUserLoginInfoGuard(router: Router) {
   router.beforeEach(async (to, from, next) => {
     NProgress.start();
     const userStore = useUserStore();
+    /**
+     * 特殊跳转配置
+     * 因为多次跳转会丢失路由信息，需要通过redirectedFrom记录
+     */
+    if (
+      ['article', 'spot', 'userPreview', 'userProfile'].includes(
+        from?.name as string
+      ) &&
+      to.name === 'login'
+    ) {
+      to.redirectedFrom = from;
+    }
+
     if (isLogin()) {
       if (userStore.role) {
         next();
@@ -26,18 +40,17 @@ export default function setupUserLoginInfoGuard(router: Router) {
           });
         }
       }
+    } else if (to.name === 'login') {
+      const isRegister = from.name === 'register';
+      to.query = {
+        redirect: isRegister ? 'home' : from.name,
+        ...to.query,
+      } as LocationQuery;
+      next();
     } else {
-      if (to.name === 'login') {
-        next();
-        return;
-      }
-      next({
-        name: 'login',
-        query: {
-          redirect: to.name,
-          ...to.query,
-        } as LocationQueryRaw,
-      });
+      const { requiresAuth } = to.meta;
+      if (requiresAuth) next({ name: 'login' });
+      else next();
     }
   });
 }
