75fb8ece18a31254f7c695992c63708cc7d54d3c
azin-cn <Mr.MF@foxmail.com>
2023-05-04 17:01:57 +0800

    feat: add vant ui

--- packages/mobile/src/main.ts
+++ packages/mobile/src/main.ts
@@ -8,15 +8,21 @@ import i18n from './locale';
 import directive from './directive';
 import './mock';
 import App from './App.vue';
-// Styles are imported via arco-plugin. See config/plugin/arcoStyleImport.ts in the directory for details
-// 样式通过 arco-plugin 插件导入。详见目录文件 config/plugin/arcoStyleImport.ts
-// https://arco.design/docs/designlab/use-theme-package
-import '@/assets/style/global.less';
-import '@/api/interceptor';
+
 // vant ui global css
 import 'vant/es/toast/style/index.mjs';
 import 'vant/es/dialog/style/index.mjs';
 import 'vant/es/image-preview/style/index.mjs';
+
+// Styles are imported via arco-plugin. See config/plugin/arcoStyleImport.ts in the directory for details
+// 样式通过 arco-plugin 插件导入。详见目录文件 config/plugin/arcoStyleImport.ts
+// https://arco.design/docs/designlab/use-theme-package
+import '@/api/interceptor';
+import '@/assets/style/global.less';
+import '@/assets/style/variables.less';
+
+// uno
+// import 'virtual:uno.css';
 
 const app = createApp(App);
 
