1d25e1fac37bfa97e7b23c0b612c227198e0fc24
azin-cn <Mr.MF@foxmail.com>
2023-05-05 11:36:16 +0800

    feat: update article card component

--- packages/mobile/src/views/components/article-card/index.vue
+++ packages/mobile/src/views/components/article-card/index.vue
@@ -1,5 +1,29 @@
 <script lang="ts" setup>
-  import { ref } from 'vue';
+  import { ArticleBriefInfo } from '@/api/article';
+  import { useClipboard } from '@vueuse/core';
+  import useArticleCard from './use-article-card';
+
+  export interface IProps {
+    article: ArticleBriefInfo;
+  }
+
+  const props = defineProps<IProps>();
+
+  const { copy } = useClipboard();
+  const { onArticleShare, onThumbsUp } = useArticleCard();
+
+  const onIconClick = async (id: string, type: 'thumb-up' | 'share') => {
+    switch (type) {
+      case 'thumb-up':
+        onThumbsUp(id);
+        break;
+      case 'share':
+        onArticleShare(id);
+        break;
+      default:
+        break;
+    }
+  };
 </script>
 
 <template>
@@ -7,14 +31,29 @@
     <!-- 个人信息 -->
     <a-row class="text-center pt-1 pb-1" align="center">
       <a-col :span="4">
-        <a-avatar>A</a-avatar>
+        <a-avatar :image-url="article.author.avatar" />
       </a-col>
       <a-col :span="16" class="text-left">
         <a-row>
-          <a-col :span="12" class="font-bold truncate">人间不值得</a-col>
-          <a-col :span="12" class="truncate">官方精选讲师</a-col>
+          <a-col :span="8" class="font-bold truncate">
+            {{ article.author.username }}
+          </a-col>
+          <a-col :span="12" class="truncate text-sm">
+            <span
+              class="inline-block p-1"
+              style="
+                color: var(--color-white);
+                background-color: var(--itravel-secondary-color);
+                border-radius: var(--itravel-third-radius);
+              "
+            >
+              {{ article.author.title || '人间不值得' }}
+            </span>
+          </a-col>
         </a-row>
-        <a-row class="text-xs">{{ new Date().toLocaleDateString() }}</a-row>
+        <a-row class="text-xs" style="color: var(--itravel-text-third-color)">
+          {{ new Date(article.publishTime).toLocaleDateString() }}
+        </a-row>
       </a-col>
       <a-col :span="4" class="text-sm"> <span>关注</span> </a-col>
     </a-row>
@@ -24,7 +63,7 @@
       <a-col :span="22">
         <a-row class="mt-1 mb-1">
           <a-col :span="14" class="truncate font-bold">
-            朱自清《荷塘月色》片段
+            {{ article.title }}
           </a-col>
         </a-row>
         <a-row style="text-indent: 2rem">
@@ -35,7 +74,7 @@
             }"
             class="text-sm mt-2 mb-2"
           >
-            路上只我一个人，背着手踱着。这一片天地好像是我的;我也像超出了平常旳自己，到了另一世界里。我爱热闹，也爱冷静;爱群居，也爱独处。像今晚上，一个人在这苍茫旳月下，什么都可以想，什么都可以不想，便觉是个自由的人。白天里一定要做的事，一定要说的话，现在都可不理。这是独处的妙处，我且受用这无边的荷香月色好了。
+            {{ article.content }}
           </a-typography-text>
         </a-row>
         <a-row>
@@ -43,9 +82,9 @@
             width="100%"
             height="170px"
             fit="cover"
-            alt="图片描述"
+            :alt="article.title"
             show-loader
-            src="https://p1-arco.byteimg.com/tos-cn-i-uwbnlip3yd/a8c8cdb109cb051163646151a4a5083b.png~tplv-uwbnlip3yd-webp.webp"
+            :src="article.thumbUrl || article.spot.thumbUrl"
             style="border-radius: var(--itravel-secondary-radius)"
           />
         </a-row>
@@ -59,15 +98,9 @@
           <a-col :span="12">
             <a-row align="center">
               <a-avatar-group :size="24" :max-count="3" z-index-ascend>
-                <a-avatar :style="{ backgroundColor: '#7BC616' }">A</a-avatar>
-                <a-avatar :style="{ backgroundColor: '#14C9C9' }">B</a-avatar>
-                <a-avatar :style="{ backgroundColor: '#168CFF' }">C</a-avatar>
-                <a-avatar :style="{ backgroundColor: '#FF7D00' }"
-                  >Arco</a-avatar
-                >
-                <a-avatar :style="{ backgroundColor: '#FFC72E' }">
-                  Design
-                </a-avatar>
+                <a-avatar :style="{ backgroundColor: '#14C9C9' }"> A </a-avatar>
+                <a-avatar :style="{ backgroundColor: '#FF7D00' }"> B </a-avatar>
+                <a-avatar :style="{ backgroundColor: '#FFC72E' }"> C </a-avatar>
               </a-avatar-group>
               <span class="align-middle">等喜欢</span>
             </a-row>
@@ -81,10 +114,24 @@
               class="float-right"
               style="color: var(--itravel-text-third-color)"
             >
-              <IconFont type="icon-xihuan" :size="16"></IconFont>
-              <IconFont type="icon-shoucang" :size="16"></IconFont>
-              <IconFont type="icon-pinglun" :size="16"></IconFont>
-              <IconFont type="icon-map-pin-range-fill" :size="16"></IconFont>
+              <IconFont
+                type="icon-good"
+                :size="16"
+                class="icon-click"
+                @click.stop="onIconClick(article.id, 'thumb-up')"
+              />
+              <IconFont
+                type="icon-zhuanfa2"
+                :size="14"
+                class="icon-click"
+                @click.stop="onIconClick(article.id, 'share')"
+              />
+              <IconFont type="icon-pinglun" :size="16" class="icon-click" />
+              <IconFont
+                type="icon-map-pin-range-fill"
+                :size="16"
+                class="icon-click"
+              />
             </a-space>
           </a-col>
         </a-row>
@@ -120,5 +167,16 @@
     padding: 16px 4px;
     border-radius: 12px;
     box-shadow: 0 0 12px 2px rgba(131, 131, 131, 0.2);
+  }
+
+  .icon-click {
+    transition: all 0.3s;
+  }
+
+  .icon-click:active {
+    transform: scale(1.2);
+
+    /* 解决active时长太短问题 */
+    transition: 0s;
   }
 </style>
