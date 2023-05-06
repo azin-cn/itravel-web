80e9924a149ce0247bd39c1814bfdd6500a7dca2
azin-cn <Mr.MF@foxmail.com>
2023-05-06 17:19:14 +0800

    refactor: update data-panel component

--- packages/admin/src/views/dashboard/workplace/components/data-panel.vue
+++ packages/admin/src/views/dashboard/workplace/components/data-panel.vue
@@ -1,3 +1,21 @@
+<script lang="ts" setup>
+  import { WorkplacePanelCount } from '@/api/dashboard';
+
+  export interface IProps {
+    counter?: Partial<WorkplacePanelCount>;
+  }
+
+  const props = withDefaults(defineProps<IProps>(), {
+    counter: () => ({
+      spotCount: 0,
+      articleCount: 0,
+      commentCount: 0,
+      todayCommentCount: 0,
+      yesterdayCommentCount: 0,
+    }),
+  });
+</script>
+
 <template>
   <a-grid :cols="24" :row-gap="16" class="panel">
     <a-grid-item
@@ -5,40 +23,14 @@
       :span="{ xs: 12, sm: 12, md: 12, lg: 12, xl: 12, xxl: 6 }"
     >
       <a-space>
-        <a-avatar :size="54" class="col-avatar">
-          <img
-            alt="avatar"
-            src="//p3-armor.byteimg.com/tos-cn-i-49unhts6dw/288b89194e657603ff40db39e8072640.svg~tplv-49unhts6dw-image.image"
-          />
+        <a-avatar image-url="" :size="54" class="col-avatar">
+          <IconFont type="icon-lvyou4" :size="28"></IconFont>
         </a-avatar>
+        <!-- 当设置百分比时，如果直接传入数据，会导致数据不渲染 -->
+        <!-- :precision="1" -->
         <a-statistic
-          :title="$t('workplace.onlineContent')"
-          :value="373.5"
-          :precision="1"
-          :value-from="0"
-          animation
-          show-group-separator
-        >
-          <template #suffix>
-            W+ <span class="unit">{{ $t('workplace.pecs') }}</span>
-          </template>
-        </a-statistic>
-      </a-space>
-    </a-grid-item>
-    <a-grid-item
-      class="panel-col"
-      :span="{ xs: 12, sm: 12, md: 12, lg: 12, xl: 12, xxl: 6 }"
-    >
-      <a-space>
-        <a-avatar :size="54" class="col-avatar">
-          <img
-            alt="avatar"
-            src="//p3-armor.byteimg.com/tos-cn-i-49unhts6dw/fdc66b07224cdf18843c6076c2587eb5.svg~tplv-49unhts6dw-image.image"
-          />
-        </a-avatar>
-        <a-statistic
-          :title="$t('workplace.putIn')"
-          :value="368"
+          title="线上景点数"
+          :value="counter.spotCount || 0"
           :value-from="0"
           animation
           show-group-separator
@@ -57,12 +49,33 @@
         <a-avatar :size="54" class="col-avatar">
           <img
             alt="avatar"
-            src="//p3-armor.byteimg.com/tos-cn-i-49unhts6dw/77d74c9a245adeae1ec7fb5d4539738d.svg~tplv-49unhts6dw-image.image"
+            src="//p3-armor.byteimg.com/tos-cn-i-49unhts6dw/fdc66b07224cdf18843c6076c2587eb5.svg~tplv-49unhts6dw-image.image"
           />
         </a-avatar>
         <a-statistic
-          :title="$t('workplace.newDay')"
-          :value="8874"
+          title="线上文章数"
+          :value="counter.articleCount || 0"
+          :value-from="0"
+          animation
+          show-group-separator
+        >
+          <template #suffix>
+            <span class="unit">{{ $t('workplace.pecs') }}</span>
+          </template>
+        </a-statistic>
+      </a-space>
+    </a-grid-item>
+    <a-grid-item
+      class="panel-col"
+      :span="{ xs: 12, sm: 12, md: 12, lg: 12, xl: 12, xxl: 6 }"
+    >
+      <a-space>
+        <a-avatar :size="54" class="col-avatar">
+          <IconFont type="icon-wenzhang1" :size="28"></IconFont>
+        </a-avatar>
+        <a-statistic
+          title="日新增文章"
+          :value="counter.todayArticleCount || 0"
           :value-from="0"
           animation
           show-group-separator
@@ -86,13 +99,59 @@
           />
         </a-avatar>
         <a-statistic
-          :title="$t('workplace.newFromYesterday')"
-          :value="2.8"
-          :precision="1"
+          title="昨日新增文章"
+          :value="counter.yesterdayArticleCount || 0"
           :value-from="0"
           animation
         >
-          <template #suffix> % <icon-caret-up class="up-icon" /> </template>
+          <template #suffix>
+            <span class="unit">{{ $t('workplace.pecs') }}</span>
+          </template>
+        </a-statistic>
+      </a-space>
+    </a-grid-item>
+    <a-grid-item
+      class="panel-col"
+      :span="{ xs: 12, sm: 12, md: 12, lg: 12, xl: 12, xxl: 6 }"
+    >
+      <a-space>
+        <a-avatar :size="54" class="col-avatar">
+          <img
+            alt="avatar"
+            src="//p3-armor.byteimg.com/tos-cn-i-49unhts6dw/77d74c9a245adeae1ec7fb5d4539738d.svg~tplv-49unhts6dw-image.image"
+          />
+        </a-avatar>
+        <a-statistic
+          title="日新增评论"
+          :value="counter.todayCommentCount || 0"
+          :value-from="0"
+          animation
+          show-group-separator
+        >
+          <template #suffix>
+            <span class="unit">{{ $t('workplace.pecs') }}</span>
+          </template>
+        </a-statistic>
+      </a-space>
+    </a-grid-item>
+    <a-grid-item
+      class="panel-col"
+      :span="{ xs: 12, sm: 12, md: 12, lg: 12, xl: 12, xxl: 6 }"
+      style="border-right: none"
+    >
+      <a-space>
+        <a-avatar :size="54" class="col-avatar">
+          <IconFont type="icon-icon_pinglun" :size="28"></IconFont>
+        </a-avatar>
+        <a-statistic
+          title="昨日新增评论"
+          :value="counter.yesterdayCommentCount"
+          :value-from="0"
+          animation
+        >
+          <template #suffix>
+            <span class="unit">{{ $t('workplace.pecs') }}</span>
+          </template>
         </a-statistic>
       </a-space>
     </a-grid-item>
@@ -101,8 +160,6 @@
     </a-grid-item>
   </a-grid>
 </template>
-
-<script lang="ts" setup></script>
 
 <style lang="less" scoped>
   .arco-grid.panel {
