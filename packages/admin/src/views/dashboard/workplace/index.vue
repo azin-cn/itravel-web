4562f2e19a078d0901581e9c2c4e091e7e77cfcb
azin-cn <Mr.MF@foxmail.com>
2023-05-06 17:19:44 +0800

    feat: use get workspace data method

--- packages/admin/src/views/dashboard/workplace/index.vue
+++ packages/admin/src/views/dashboard/workplace/index.vue
@@ -1,15 +1,26 @@
 <script lang="ts" setup>
-  import { queryContentData } from '@/api/dashboard';
+  import { ref } from 'vue';
+  import {
+    getWorkspaceCounter,
+    queryContentData,
+    WorkplaceModel,
+  } from '@/api/dashboard';
   import Banner from './components/banner.vue';
   import DataPanel from './components/data-panel.vue';
   import ContentChart from './components/content-chart.vue';
-  import PopularContent from './components/popular-content.vue';
-  import CategoriesPercent from './components/categories-percent.vue';
   import RecentlyVisited from './components/recently-visited.vue';
   import QuickOperation from './components/quick-operation.vue';
   import Announcement from './components/announcement.vue';
   import Carousel from './components/carousel.vue';
   import Docs from './components/docs.vue';
+
+  const workspaceData = ref<Partial<WorkplaceModel>>({});
+
+  const init = async () => {
+    const [{ data: counter }] = await Promise.all([getWorkspaceCounter()]);
+    workspaceData.value.counter = counter;
+  };
+  init();
 </script>
 
 <script lang="ts">
@@ -23,21 +34,10 @@
     <div class="left-side">
       <div class="panel">
         <Banner />
-        <DataPanel />
+        <DataPanel :counter="workspaceData.counter" />
+        <ContentChart />
         <ContentChart />
       </div>
-      <a-grid :cols="24" :col-gap="16" :row-gap="16" style="margin-top: 16px">
-        <a-grid-item
-          :span="{ xs: 24, sm: 24, md: 24, lg: 12, xl: 12, xxl: 12 }"
-        >
-          <PopularContent />
-        </a-grid-item>
-        <a-grid-item
-          :span="{ xs: 24, sm: 24, md: 24, lg: 12, xl: 12, xxl: 12 }"
-        >
-          <CategoriesPercent />
-        </a-grid-item>
-      </a-grid>
     </div>
     <div class="right-side">
       <a-grid :cols="24" :row-gap="16">
