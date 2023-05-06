61dc6aaa8db03ab9f9049dd975f0606283015536
azin-cn <Mr.MF@foxmail.com>
2023-05-06 08:27:42 +0800

    feat: add data panel type

--- packages/admin/src/api/dashboard.ts
+++ packages/admin/src/api/dashboard.ts
@@ -1,6 +1,33 @@
 import axios from 'axios';
 import type { TableData } from '@arco-design/web-vue/es/table/interface';
 
+export interface WorkplacePanelCount {
+  /**
+   * 景点数量
+   */
+  spotCount: number;
+  /**
+   * 文章数量
+   */
+  articleCount: number;
+  /**
+   * 评论总数量
+   */
+  commentCount: number;
+  /**
+   * 日新增数量
+   */
+  todayCommentCount: number;
+  /**
+   * 昨日新增数量
+   */
+  yesterdayCommentCount: number;
+}
+
+export interface WorkplaceModel {
+  banner: WorkplacePanelCount;
+}
+
 export interface ContentDataRecord {
   x: string;
   y: number;
