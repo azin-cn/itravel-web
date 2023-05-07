d78c2d692877d98d4adce7330e88fc228ead8b99
azin-cn <Mr.MF@foxmail.com>
2023-05-07 19:42:20 +0800

    feat: update search api

--- packages/admin/src/api/list.ts
+++ packages/admin/src/api/list.ts
@@ -1,6 +1,8 @@
 import axios from 'axios';
 import qs from 'query-string';
 import type { DescData } from '@arco-design/web-vue/es/descriptions/interface';
+import { ListResult } from '@/types/global';
+import { BaseModel, prefix } from './base';
 
 export interface PolicyRecord {
   id: string;
@@ -53,4 +55,45 @@ export function queryTheServiceList() {
 
 export function queryRulesPresetList() {
   return axios.get('/api/list/rules-preset');
+}
+
+export interface ISpotQueryParams {
+  id: string;
+  name: string;
+  region: string;
+  create_date_before: string;
+  create_date_after: string;
+}
+
+export interface RegionModel {
+  id: string;
+  name: string;
+  fullName: string;
+  weight: number;
+  aid: string;
+  bid: string;
+  tid: string;
+}
+
+export interface AdminSpotModel {
+  id: string;
+  name: string;
+  description: string;
+  thumbUrl: string;
+  panorama: string;
+  weight: number;
+  months: BaseModel[];
+  features: BaseModel[];
+  country: RegionModel;
+  province: RegionModel;
+  city: RegionModel;
+  district: RegionModel;
+  createdTime: string;
+  updatedTime: string;
+}
+
+export function getSpotsByConditions(params: ISpotQueryParams) {
+  return axios.get<ListResult<AdminSpotModel>>(`${prefix}/admin/spot/query`, {
+    params,
+  });
 }
