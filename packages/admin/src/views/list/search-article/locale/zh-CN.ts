17882510f533fe43b4ce786bd4485d7670e88e54
azin-cn <Mr.MF@foxmail.com>
2023-05-17 22:24:01 +0800

    feat: update text

--- packages/admin/src/views/list/search-article/locale/zh-CN.ts
+++ packages/admin/src/views/list/search-article/locale/zh-CN.ts
@@ -1,6 +1,5 @@
 export default {
   'menu.list.searchArticle': '文章数据',
   'list.toolbar.create': '新建',
-  'list.columns.operations.preview': '预览景点',
-  'list.columns.operations.preview3d': '预览3D',
+  'list.columns.operations.preview': '预览文章',
 };
